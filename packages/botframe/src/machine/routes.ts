9900c2bbf4b9df7af6182a1cae369be46b94d729
Jan Hein Hoogstad <yeehaa@offcourse.io>
2020-01-15 14:07:37 +0100

    sample recommendation handlers

--- packages/botframe/src/machine/routes.ts
+++ packages/botframe/src/machine/routes.ts
@@ -8,8 +8,18 @@ const sayGoodbye = async (context: any) => {
   await context.sendText("Farewell World");
 };
 
+const getRecommendations = async (_tweet: string) => {
+  return ["one", "two", "three"];
+};
+
+const recommend = async (context: any) => {
+  const recommendations = getRecommendations(context.event.text);
+  await context.sendText(JSON.stringify(recommendations));
+};
+
 const routes = [
   text(["Hi", "Hello"], sayHello),
+  text(/hashtag.+recommend/, recommend),
   text(["Bye", "Goodbye"], sayGoodbye)
 ];
 
