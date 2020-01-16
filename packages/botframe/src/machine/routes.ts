43e9a6af84f2f696d6cd5e5db8a5ab97394c862a
Scott <schepers.scott@hotmail.com>
2020-01-16 14:44:35 +0100

    Changed routes

--- packages/botframe/src/machine/routes.ts
+++ packages/botframe/src/machine/routes.ts
@@ -1,35 +1,55 @@
 import context from "./defaultContext";
 
 const { text } = require("bottender/router");
-const Knn = require('./TweetIt/KNN.js')
+const Knn = require("./TweetIt/KNN.js");
+
+let tweet = false;
 
 const sayHello = async (context: any) => {
-  await context.sendText("Hello World");
+	await context.sendText("Hello World");
 };
 
 const sayGoodbye = async (context: any) => {
-  await context.sendText("Farewell World");
+	await context.sendText("Farewell World");
 };
 
 const getRecommendations = async (_tweet: string) => {
-  const knn = new Knn("yeehaa");
-  const neighbors = 4;
-  console.log("getting recommendations for tweet: ", _tweet)
-  let result = await knn.getRecommendation(_tweet, neighbors)
-  result = result.slice(0,3)
-  result = result[0].key + " " + result[1].key + " " + result[2].key
-  return result;
+	const knn = new Knn("yeehaa");
+	const neighbors = 4;
+	console.log("getting recommendations for tweet: ", _tweet);
+	let result = await knn.getRecommendation(_tweet, neighbors);
+	const resultsAmount = 5;
+	let string = "";
+	for (let index = 0; index < resultsAmount; index++) {
+		string += result[index].key + " ";
+	}
+	return string;
 };
 
 const recommend = async (context: any) => {
-  const recommendations = await getRecommendations(context.event.text);
-  await context.sendText(recommendations);
+	if (tweet) {
+		const recommendations = await getRecommendations(context.event.text);
+		tweet = false;
+		await context.sendText(recommendations);
+	}
+	return;
+};
+
+const askTweet = async (context: any) => {
+	if (!tweet) {
+		await context.sendText(
+			"What is the tweet you want hashtags to be recommended for?"
+		);
+    tweet = true;
+	}
+	return;
 };
 
 const routes = [
-  text(["Hi", "Hello"], sayHello),
-  text(/hashtag.+recommend/, recommend),
-  text(["Bye", "Goodbye"], sayGoodbye)
+	text(["Hi", "Hello"], sayHello),
+	text(/hashtag.+recommend/, askTweet),
+	text(["Bye", "Goodbye"], sayGoodbye),
+	text(/.+/, recommend)
 ];
 
 export default routes;
