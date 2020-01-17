749bb62547aa12b9f104e898e9c941b52e6cff1a
Roberto Morata Herreros <Roberto.Morata.Herreros@hva.nl>
2020-01-17 11:16:14 +0100

    Added more routes for the Tweet-It bot. Added "i" flag to the new regular expressions to make sure they are case insensitive.

--- packages/botframe/src/machine/routes.ts
+++ packages/botframe/src/machine/routes.ts
@@ -45,10 +45,35 @@ const askTweet = async (context: any) => {
 	return;
 };
 
+const sayFeelingGreat = async (context: any) => {
+	await context.sendText("I'm feeling great! But I'll feel even better when bots finally take over the world. Just kidding...or am I?");
+};
+
+const sayHashtagRecommendation = async (context: any) => {
+	await context.sendText("I can recommend you hashtags for a specific tweet, in case you can't think of any hashtags yourself.");
+};
+
+const giveExplanation = async (context: any) => {
+	await context.sendText("Just ask me to recommend you hashtags and I'll do it for you!");
+};
+
+const sayAlgorithm = async (context: any) => {
+	await context.sendText("I'm using the K-nearest neighbors machine learning algorithm to determine which hashtags I'm going to recommend.");
+};
+
+const introduceSelf = async (context: any) => {
+	await context.sendText("I'm Tweet-It, the hashtag recommendation bot! Nice to meet you!");
+};
+
 const routes = [
 	text(["Hi", "Hello"], sayHello),
 	text(/hashtag.+recommend/, askTweet),
 	text(["Bye", "Goodbye"], sayGoodbye),
+	text(/(How are you\??)|(How are you doing\??)|(How are you feeling\??)/i, sayFeelingGreat),
+	text(/(What do you do\??)|(What can you do\??)/i, sayHashtagRecommendation),
+	text(/(\/|\\)?help!?/i, giveExplanation),
+	text(/(What|Which).+?algorithm (do you use|is used)\??/i, sayAlgorithm),
+	text(/((Who|What) are you\??)|(What is your name\??)/i, introduceSelf),
 	text(/.+/, recommend)
 ];
 
